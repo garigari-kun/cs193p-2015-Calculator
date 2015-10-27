3b0a29815310c81fb0b28a1cf98a1c7ed3539ec1
kei-cs <keisuke.cs@gmail.com>
2015-10-27 21:28:04 +0900

    Assignment01 Required Task

--- KEICalculator/CalculatorViewController.swift
+++ KEICalculator/CalculatorViewController.swift
@@ -12,23 +12,42 @@ class CalculatorViewController: UIViewController
 {
     
     @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var historyLabel: UILabel!
+    
     
     var userIsInTheMiddleOfTypingANumber = false
+    
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTypingANumber {
+            /*
+            if digit == "." && userHasEnteredDecimal == true {
+                return
+            } else if digit == "." && userHasEnteredDecimal == false {
+                userHasEnteredDecimal = true
+            }
+            */
+            if digit == "." && displayLabel.text!.rangeOfString(".") != nil {
+                return
+            }
             displayLabel.text = displayLabel.text! + digit
         } else {
-            displayLabel.text = digit
             userIsInTheMiddleOfTypingANumber = true
+            if digit == "." {
+                displayLabel.text = "0."
+            } else {
+                displayLabel.text = digit
+            }
         }
     }
     
     var operandStack = Array<Double>()
     
+    
     @IBAction func operate(sender: UIButton) {
         let operation = sender.currentTitle!
+        trackHistory(operation)
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
@@ -39,9 +58,14 @@ class CalculatorViewController: UIViewController
             case "+": performBinaryOperation() { $0 + $1 }
             case "−": performBinaryOperation() { $1 - $0 }
             case "√": performUnaryOperation() { sqrt($0) }
+            case "sin": performUnaryOperation() { sin($0) }
+            case "cos": performUnaryOperation() { cos($0) }
+            case "π": performConstants(operation)
             default: break
         }
     }
+    
+    
     
     func performBinaryOperation(operation: (Double, Double) -> Double) {
         if operandStack.count >= 2 {
@@ -57,11 +81,35 @@ class CalculatorViewController: UIViewController
         }
     }
     
+    
+    func performConstants(symbol: String) {
+        switch symbol {
+            case "π":
+                displayValue = M_PI
+            default: break
+        }
+    }
+    
+    func trackHistory(historyElement: String) {
+        historyLabel.text = historyLabel.text! + " " + historyElement
+    }
+    
+    
+    @IBAction func clear() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack = [Double]()
+        displayLabel.text = "0"
+        historyLabel.text = ""
+        print("C: operandStack = \(operandStack)")
+    }
+    
 
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false
         operandStack.append(displayValue)
+        trackHistory(displayLabel.text!)
         print("operandStack = \(operandStack)")
+        
     }
     
     // Computed property for appending into the stack and displaying a displayLabel
